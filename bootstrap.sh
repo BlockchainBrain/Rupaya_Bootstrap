e60824b63291f2653e91c75bcf00ff9b87cef616
Brian Bearden <35510406+BlockchainBrain@users.noreply.github.com>
2019-01-20 14:25:31 -0600

    created bootstrap

---
+++ bootstrap.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo stopping the Rupaya daemon
+rupaya-cli stop
+wait
+echo deleting the old chain files
+rm -rf ~/.rupayacore/backups ~/.rupayacore/blocks ~/.rupayacore/chainstate ~/.rupayacore/database ~/.rupayacore/sporks ~/.rupayacore/zerocoin >/dev/null 2>&1
+rm ~/.rupayacore/*.log ~/.rupayacore/*.dat ~/.rupayacore/.lock ~/.rupayacore/rupayad.pid >/dev/null 2>&1
+wait
+echo downloading the new bootstrap files
+wget https://www.dropbox.com/s/hqmmf5wo6gpbq1b/rupx-bootstrap-160119.zip  >/dev/null 2>&1
+echo download complete
+wait
+echo installing unzip
+sudo apt-get install unzip  >/dev/null 2>&1
+wait
+echo unzipping the bootstrap into the rupaya folder
+sudo unzip rupx-bootstrap-160119.zip -d ~/.rupayacore/  >/dev/null 2>&1
+sudo chown -R rupxmn:rupxmn ~/.rupayacore/
+echo Starting the Rupaya daemon
+rupayad -daemon
+echo The bootstrap update is now complete!
+exit
