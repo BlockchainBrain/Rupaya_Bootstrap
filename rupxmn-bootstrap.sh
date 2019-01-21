579fba463051970070b47faf2ffbacbea68e4379
Brian Bearden <35510406+BlockchainBrain@users.noreply.github.com>
2019-01-21 10:52:51 -0600

    Update rupxmn-bootstrap.sh

--- rupxmn-bootstrap.sh
+++ rupxmn-bootstrap.sh
@@ -21,7 +21,7 @@ echo installing Unzip
 sudo apt-get install unzip -y  >/dev/null 2>&1
 wait
 echo Unzipping the bootstrap folders and files into the ~/.rupayacore directory, this should only take 1 minute.
-echo When prompted, type A to replace the files.
+echo If prompted, type A to replace the files.
 wait
 sudo unzip rupx-bootstrap-160119.zip -d ~/.rupayacore/
 wait
