bedc189b379b117df0fc543d69984725cee4e871
Brian Bearden <35510406+BlockchainBrain@users.noreply.github.com>
2019-01-21 10:09:46 -0600

    Update rupxmn-bootstrap.sh

--- rupxmn-bootstrap.sh
+++ rupxmn-bootstrap.sh
@@ -9,7 +9,7 @@ sudo mkdir ~/.rupayacore
 sudo mv rupaya.conf ~/.rupayacore/.
 wait
 echo downloading the new bootstrap folders and files, this will take 1-2 minutes.
-wget https://www.dropbox.com/s/hqmmf5wo6gpbq1b/rupx-bootstrap-160119.zip
+wget https://www.dropbox.com/s/hqmmf5wo6gpbq1b/rupx-bootstrap-160119.zip >/dev/null 2>&1
 echo download complete
 wait
 echo installing Unzip
@@ -17,7 +17,7 @@ sudo apt-get install unzip -y  >/dev/null 2>&1
 wait
 echo unzipping the bootstrap folders and files into the ~/.rupayacore directory, this will take 1 minute.
 wait
-sudo unzip rupx-bootstrap-160119.zip -d ~/.rupayacore/
+sudo unzip rupx-bootstrap-160119.zip -d ~/.rupayacore/ >/dev/null 2>&1
 wait
 sudo chown -R rupxmn:rupxmn ~/.rupayacore/
 sudo rm rupx-bootstrap-160119.zip
