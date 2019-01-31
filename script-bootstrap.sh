7144f1a55129d4a64b5382757976d02538dd08be
Brian Bearden <35510406+BlockchainBrain@users.noreply.github.com>
2019-01-31 10:25:52 -0600

    Updated bootstrap link
    New link: https://rupaya.ams3.cdn.digitaloceanspaces.com/bootstrap/rupx-bootstrap.zip
    Old link: https://www.dropbox.com/s/hqmmf5wo6gpbq1b/rupx-bootstrap-160119.zip

--- script-bootstrap.sh
+++ script-bootstrap.sh
@@ -13,7 +13,7 @@ mv rupaya.conf ~/.rupayacore/.
 wait
 echo downloading the new bootstrap folders and files, this will take 1-2 minutes.
 wait
-wget https://www.dropbox.com/s/hqmmf5wo6gpbq1b/rupx-bootstrap-160119.zip  >/dev/null 2>&1
+wget https://rupaya.ams3.cdn.digitaloceanspaces.com/bootstrap/rupx-bootstrap.zip  >/dev/null 2>&1
 wait
 echo download complete
 wait
@@ -23,9 +23,9 @@ wait
 echo Unzipping the bootstrap folders and files into the ~/.rupayacore directory, this should only take 1 minute.
 echo If prompted, type A to replace the files.
 wait
-unzip rupx-bootstrap-160119.zip -d ~/.rupayacore/
+unzip rupx-bootstrap.zip -d ~/.rupayacore/
 wait
-sudo rm rupx-bootstrap-160119.zip
+sudo rm rupx-bootstrap.zip
 wait
 echo Starting the Rupaya Daemon
 systemctl start Rupaya.service
