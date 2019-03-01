b9a9cb996646a13d12955d7182f0945ccccd8e2a
Brian Bearden <35510406+BlockchainBrain@users.noreply.github.com>
2019-03-01 16:26:59 -0600

    Update script-bootstrap.sh
    Added the link to the new bootstrap and removed the install of unzip

--- script-bootstrap.sh
+++ script-bootstrap.sh
@@ -13,23 +13,21 @@ mv rupaya.conf ~/.rupayacore/.
 wait
 echo Downloading the new bootstrap folders and files, this will take 1-2 minutes.
 wait
-wget https://www.dropbox.com/s/hqmmf5wo6gpbq1b/rupx-bootstrap-160119.zip  >/dev/null 2>&1
+wget https://rupaya.ams3.cdn.digitaloceanspaces.com/bootstrap/rupx-bootstrap.tar.gz   >/dev/null 2>&1
 wait
 echo Download complete
-wait
-echo Installing Unzip
-apt-get install unzip -y  >/dev/null 2>&1
 wait
 echo Unzipping the bootstrap folders and files into the ~/.rupayacore directory, this should only take 1 minute.
 echo If prompted, type A to replace the files.
 wait
-unzip rupx-bootstrap-160119.zip -d ~/.rupayacore/
-wait
-sudo rm rupx-bootstrap-160119.zip
-sudo rm script-bootstrap.sh
+tar xf rupx-bootstrap.tar.gz -C ~/
 wait
 echo Starting the Rupaya Daemon
 systemctl start Rupaya.service
 echo The bootstrap installation complete and the Rupaya daemon should be running!
 echo To confirm that the bootstrap worked, run the command: rupaya-cli getinfo
+wait
+sudo rm rupx-bootstrap.tar.gz
+sudo rm root-bootstrap.sh
+wait
 exit
